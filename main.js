8fabd2dc8571019c8704eaa61eabc689448698b3
Eric Weiss <eweiss1995@gmail.com>
2018-01-27 16:25:57 -0500

    Improvements with programming section, unified font.

--- main.js
+++ main.js
@@ -1,13 +1,54 @@
-$(document).ready(function () {
-    $(".button-collapse").sideNav();
-    $('.carousel.carousel-slider').carousel({
-        fullWidth: true
-    });
-    $('.collapsible').collapsible();
+  var object = [
+      {
+          'headline': 'HTML',
+          'value': 9,
+          'length': 10,
+          'description': 'Proficcient and creating and debugging'
+        },
+      {
+          'headline': 'CSS',
+          'value': 8,
+          'length': 10,
+          'description': 'Knowledge of css'
+        },
+      {
+          'headline': 'Javascript',
+          'value': 8,
+          'length': 10,
+          'description': 'Knowledge of css'
+        },
+      {
+          'headline': 'C++',
+          'value': 7,
+          'length': 10,
+          'description': 'Knowledge of css'
+        },
+      {
+          'headline': 'Java',
+          'value': 7,
+          'length': 10,
+          'description': 'Knowledge of css'
+        },
+      {
+          'headline': 'PHP',
+          'value': 4,
+          'length': 10,
+          'description': 'Knowledge of css'
+        }
+    ];
 
-    var shiftWindow = function () {
-        scrollBy(0, -50)
-    };
-    if (location.hash) shiftWindow();
-    window.addEventListener("hashchange", shiftWindow);
-});
+  $(document).ready(function () {
+
+      $('#skillset').skillset({
+          object: object,
+          duration: 80
+      });
+
+      /* ----Materialize Functions ---- */
+      $('.button-collapse').sideNav();
+      $('.carousel.carousel-slider').carousel({
+          fullWidth: true
+      });
+      $('.collapsible').collapsible();
+
+  });
