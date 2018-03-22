bb04d2fbb9c342e60f1e86a458a4d622859ff768
Eric Weiss <eweiss1995@gmail.com>
2018-03-22 16:43:04 -0400

    Skills update description

--- main.js
+++ main.js
@@ -3,38 +3,44 @@
           'headline': 'HTML',
           'value': 9,
           'length': 10,
-          'description': 'Proficcient at base HTML. Experience with the PUG precompiler.'
+          'description': 'Extremely proficient at base HTML. Some experience with the PUG precompiler.'
         },
       {
           'headline': 'CSS',
           'value': 8,
           'length': 10,
-          'description': 'Knowledge of CSS.  Experience in frameworks bootstrap and materialize. Fair experience using SCSS.'
+          'description': 'Advanced Knowledge of CSS.  Experience in the frameworks bootstrap and materialize. Fair experience using SCSS.'
         },
       {
           'headline': 'Javascript',
-          'value': 8,
-          'length': 10,
-          'description': 'Intermediate to advanced knowledge of Javascript and JQuery. Novice experience in ReactJS and Node.js,'
-        },
-      {
-          'headline': 'C++',
           'value': 7,
           'length': 10,
-          'description': 'Intermediate experience. Peer mentor for beginner class in college.'
+          'description': 'Intermediate to advanced knowledge of Javascript and JQuery.'
         },
       {
-          'headline': 'Java',
-          'value': 7,
+            'headline': 'Java',
+          'value': 6,
           'length': 10,
           'description': 'Intermediate experience. Have done work in SeLion, an extension of Selenium automated testing.'
+        },
+      {
+         'headline': 'C++',
+          'value': 5,
+          'length': 10,
+          'description': 'Intermediate experience. Peer mentor for beginner class in college.'
         },
       {
           'headline': 'PHP',
           'value': 4,
           'length': 10,
-          'description': 'Base knowledge. Have used to access mySql database for design project.'
-        }
+          'description': 'Basic knowledge. Have used to access mySQL database in senior design project.'
+      },
+      {
+          'headline': 'ReactJS',
+          'value': 2,
+          'length': 10,
+          'description': 'Getting my feet wet with this one. Doing tutorials and self projects to advance my learning.'
+      }
     ];
 
   $(document).ready(function () {
