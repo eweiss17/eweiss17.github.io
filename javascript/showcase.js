6a1d5c18822daf647c943c730ccfe4177f0b9154
Eric Weiss <eweiss1995@gmail.com>
2019-04-06 19:24:37 -0400

    seperating js files

---
+++ javascript/showcase.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+     $('.carousel.carousel-slider').carousel({
+          fullWidth: true
+      });
+
+      setTimeout(autoplay, 7500);
+
+      function autoplay() {
+          $('.carousel').carousel('next');
+          setTimeout(autoplay, 7500);
+      }
+
+      $('.carousel').tooltip({
+        content: "Click to go to!"
+      });
+});
---
+++ javascript/skillset_custom.js
@@ -0,0 +1,51 @@
+  var skills = [
+    {
+      'headline': 'Web Development',
+      'value': 8,
+      'length': 10,
+      'description': 'Know the ins and outs of web development, including HTML, CSS and Javascript'
+    },
+    {
+      'headline': 'PHP',
+      'value': 6,
+      'length': 10,
+      'description': 'Moderate knowledge. Have worked in a legacy codebase.'
+    },
+    {
+      'headline': 'Java',
+      'value': 6,
+      'length': 10,
+      'description': 'Intermediate experience. Have done work in SeLion, an extension of Selenium automated testing.'
+    },
+    {
+      'headline': 'Ruby on rails',
+      'value': 5,
+      'length': 10,
+      'description': 'Intermediate experience. Have worked on several projects utilizing this.'
+    },
+    {
+      'headline': 'C++',
+      'value': 5,
+      'length': 10,
+      'description': 'Intermediate experience. Peer mentor for beginner class in college.'
+    },
+    {
+      'headline': 'WordPress',
+      'value': 4,
+      'length': 10,
+      'description': 'Intermediate experience. Several projects completed in WordPress.'
+    },
+    {
+      'headline': 'ReactJS',
+      'value': 2,
+      'length': 10,
+      'description': 'Getting my feet wet with this one. Doing tutorials and self projects to advance my learning.'
+    }
+];
+
+$(document).ready(function () {
+    $('#skillset').skillset({
+      object: skills,
+      duration: 80
+    });
+});
