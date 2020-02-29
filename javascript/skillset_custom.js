c190bdb0ba76eea1ed22700b58eb0469c325639a
Eric Weiss <eweiss1995@gmail.com>
2020-02-29 15:17:47 -0500

    adding react native and peach project

--- javascript/skillset_custom.js
+++ javascript/skillset_custom.js
@@ -7,21 +7,39 @@
     },
     {
       'headline': 'PHP',
+      'value': 7,
+      'length': 10,
+      'description': 'Moderate knowledge to experienced. Have worked in a several PHP codebases.'
+    },
+    {
+      'headline': 'mySQL',
+      'value': 7,
+      'length': 10,
+      'description': 'Am able to solve complex problems with.'
+    },
+    {
+      'headline': 'React',
       'value': 6,
       'length': 10,
-      'description': 'Moderate knowledge. Have worked in a legacy codebase.'
+      'description': 'Have done multiple projects with React and have have a intemediate base understanding.'
+    },
+    {
+      'headline': 'React Native',
+      'value': 6,
+      'length': 10,
+      'description': 'Have constructed multiple projects from the ground up. Moderately experienced.'
+    },
+    {
+      'headline': 'Ruby on rails',
+      'value': 6,
+      'length': 10,
+      'description': 'Intermediate experience. Have worked on several projects utilizing this.'
     },
     {
       'headline': 'Java',
       'value': 6,
       'length': 10,
       'description': 'Intermediate experience. Have done work in SeLion, an extension of Selenium automated testing.'
-    },
-    {
-      'headline': 'Ruby on rails',
-      'value': 5,
-      'length': 10,
-      'description': 'Intermediate experience. Have worked on several projects utilizing this.'
     },
     {
       'headline': 'C++',
@@ -31,15 +49,15 @@
     },
     {
       'headline': 'WordPress',
-      'value': 4,
+      'value': 5,
       'length': 10,
       'description': 'Intermediate experience. Several projects completed in WordPress.'
     },
     {
-      'headline': 'ReactJS',
-      'value': 2,
+      'headline': 'Firestore',
+      'value': 5,
       'length': 10,
-      'description': 'Getting my feet wet with this one. Doing tutorials and self projects to advance my learning.'
+      'description': 'Understand this noSQL database and am able to work with without issue.'
     }
 ];
 
